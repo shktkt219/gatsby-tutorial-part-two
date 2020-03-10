345a2a2d12a2a888fd727998b6a1179fb27095ae
shktkt219 <shktkt219@gmail.com>
2020-03-10 07:37:30 +0200

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,67 @@
+import React from "react"
+
+export default () => (
+   <div>
+        <h2>
+            <span>ー</span><br/>
+            <br/>
+            <br/>
+            <span>New York</span><br/>
+            <span>Design Strategies</span><br/>
+            <span>for Japanese Business:</span><br/>
+            <span>Winning in the Global Market</span>
+        </h2>
+        <h1>
+            <span>ニューヨークの</span><br/>
+            <span>アートディレクターがいま、</span><br/>
+            <span>日本のビジネスリーダーに</span><br/>
+            <span>伝えたいこと</span>
+        </h1>
+        <h3>
+            世界に通用するデザイン経営戦略
+        </h3>
+        <p>
+            小山田 育 ｜ 渡邊 デルーカ 瞳
+        </p>
+        <h4>Amazonで購入</h4>
+
+        <h2>
+            <span>ー</span><br/>
+            <span>New York</span><br/>
+            <span>Design Strategies</span><br/>
+            <span>for Japanese Business:</span><br/>
+            <span>Winning in the Global Market</span>
+        </h2>
+        <h1>
+            <span>ニューヨークの</span><br/>
+            <span>アートディレクターがいま、</span><br/>
+            <span>日本のビジネスリーダーに</span><br/>
+            <span>伝えたいこと</span>
+        </h1>
+        <h3>
+            世界に通用するデザイン経営戦略
+        </h3>
+        <p>
+            小山田 育 ｜ 渡邊 デルーカ 瞳
+        </p>
+        <h2>
+            <span>ー</span><br/>
+            <span>New York</span><br/>
+            <span>Design Strategies</span><br/>
+            <span>for Japanese Business:</span><br/>
+            <span>Winning in the Global Market</span>
+        </h2>
+        <h1>
+            <span>ニューヨークの</span><br/>
+            <span>アートディレクターがいま、</span><br/>
+            <span>日本のビジネスリーダーに</span><br/>
+            <span>伝えたいこと</span>
+        </h1>
+        <h3>
+            世界に通用するデザイン経営戦略
+        </h3>
+        <p>
+            小山田 育 ｜ 渡邊 デルーカ 瞳
+        </p>
+   </div>
+)
